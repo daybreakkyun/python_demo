384cca476076b22af2e5c0dd5b4ba2def4ebc616
Zac Daniel <daybreakkyun@gmail.com>
2023-10-23 22:21:19 +0800

    Changed department schema

--- modules/department/department_schema.py
+++ modules/department/department_schema.py
@@ -1,8 +1,16 @@
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from marshmallow_enum import EnumField
 from marshmallow import fields
+from modules.person.person_model import Person, Gender
 from modules.department.department_model import Department, DepartmentCode
-from modules.person.person_schema import PersonSchema
+
+class PersonSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Person
+        load_instance = True
+    name = fields.Str(required=True)
+    birth_date = fields.Date(format="%Y-%m-%d", required=True)
+    gender = EnumField(Gender, by_value=True, required=True)
 
 class DepartmentSchema(SQLAlchemyAutoSchema):
     class Meta:
@@ -13,7 +21,7 @@ class DepartmentSchema(SQLAlchemyAutoSchema):
     code = EnumField(DepartmentCode, by_value=True, required=True)
     persons = fields.List(
         fields.Nested(
-            PersonSchema(exclude=["birth_date", "gender", "department_id"])
+            PersonSchema(exclude=["gender", "birth_date"])
         )
     )
 
--- modules/person/person_schema.py
+++ modules/person/person_schema.py
@@ -2,6 +2,15 @@ from marshmallow import fields
 from modules.person.person_model import Person, Gender
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from marshmallow_enum import EnumField
+from modules.department.department_model import Department, DepartmentCode
+
+class DepartmentSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Department
+        load_instance = True
+    name = fields.Str(required=True)
+    description = fields.Str(required=False)
+    code = EnumField(DepartmentCode, by_value=True, required=True)
 
 class PersonSchema(SQLAlchemyAutoSchema):
     class Meta:
@@ -11,6 +20,7 @@ class PersonSchema(SQLAlchemyAutoSchema):
     birth_date = fields.Date(format="%Y-%m-%d", required=True)
     gender = EnumField(Gender, by_value=True, required=True)
     department_id = fields.Int()
+    department = fields.Nested(DepartmentSchema(exclude=["id", "description", "code"]))
 
 schema = PersonSchema()
 schema_list = PersonSchema(many=True)
